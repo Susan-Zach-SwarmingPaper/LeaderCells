875d21a4fb248a55a057efaa5e17f36ef029d2b0
selegget <susan_leggett@brown.edu>
2017-11-02 19:13:36 -0400

    leader cell code clean up
    removed older code (faulty) and kept for reference movie save and data extraction-- should make these a separate code eventually

--- leaders_test.m
+++ leaders_test.m
@@ -151,6 +151,7 @@ counter = counter + 1;
 end   
 %end
 %%
+% extract cells
 % for k = 1:size(positionFt,1)
 %     x=positionFt{k};
 % end
@@ -165,7 +166,6 @@ end
 %     test2={test2};
 % end
 % end
-%%
 
 
 % columns = size(datastoreL,2);
@@ -180,15 +180,7 @@ end
 %      positionFx = positionFt(jj(:,2),kk);
 %      positionFy = positionFt(jj(:,3),kk);
 %      end 
-% end
-%     pointsize = 200;
-% scatter(positionFx,positionFy,pointsize,cmap(1,:),'filled','green');
-% hold on
-% scatter(positionLx,positionLy,pointsize,cmap(1,:),'filled','magenta');
-% hold on
-% pointsize= 70;
-% scatter(storeXh(:,i),storeYh(:,i),pointsize,cmap(5,:),'filled');
-
+% end  
 
     
 %%
@@ -249,125 +241,5 @@ end
 
 % hold off
 % close(writrObj);
-
-
-% a = 2;
-% hBrushLine = findall(gca,'tag','Brushing');
-% brushedData = get(hBrushLine, {'Xdata','Ydata'});
-% %brushedIdx = ~isnan(brushedData{1});
-% brushedXData = brushedData{1};
-% brushedYData = brushedData{2};
-%  figure;
-%  p = plot(1:10);
-%      addlistener(p,'YData','PostSet',@(s,e)disp('Data changed'));
-%      h = brush;
-%      set(h, 'Enable', 'on') 
-% % h = brush;
-% % get(h) 
-% Find brushing object
-% bb = findall(gcf,'Tag','Brushing');
-% % Find selected points
-% i = ~isnan(get(bb,'Cdata'));
-% [x,y]=find(i);
-% % Return coordinates for these points
-% X = get(bb,'XData');
-% Y = get(bb,'YData');
-% Z = get(bb,'ZData');
-% X = X(x)
-% Y = Y(y)
-% Z = Z(i)
-% hLine = plot(...);
-% hBrushHandles = hLine.BrushHandles;
-% hBrushChildrenHandles = hBrushHandles.Children; 
-% brushedIdx = logical(hLine.BrushData);  % logical array
-% brushedXData = hLine.XData(brushedIdx);
-%  brushedYData = hLine.YData(brushedIdx)
-
-% function togglebutton1_Callback(hObject, eventdata, handles)
-% if hObject.Value == 1
-% 	brush( handles.figure1, 'on' );
-% else
-% 	brush( handles.figure1, 'off' );
-% end
-% function uitoggletool_brush_ClickedCallback(hObject, eventdata, handles)
-% 
-% if strcmp(get(handles.uitoggletool_brush, 'state'), 'on');
-%     brush( handles.figure1, 'on' ); 
-% else brush( handles.figure1, 'off' ); 
-% end
-% guidata(hObject, handles);
-% end
-% xd = get(Handle, 'XData');
-% yd = get(Handle, 'YData');
-% brush1 = get(Handle, 'BrushData');
-% brushed_x = xd(brush1);
-% brushed_y = yd(brush1);
-% ax1 = plot(rand(50,1));
-% brush1 = logical(get(ax1, 'BrushData'));
-% xd = get(ax1, 'XData');
-% yd = get(ax1, 'YData');
-% brushed_x = xd(brush1);
-% brushed_y = yd(brush1);
-% hBrush = brush(figHandles);
-%     set( hBrush,'ActionPostCallback', @(ohf, s) brushDataCallback, 'enable', 'on', 'Color', [1 0 1] )
-% hBrushLine = findall(gca,'tag','Brushing'); 
-% brushedData = get(hBrushLine, {'storeX','storeY'}); 
-% brushedIdx = ~isnan(brushedData{1}); 
-% brushedYData = brushedData{2}(brushedIdx);
-% 
-% 
-% ecgDWT1(:,1)=resampad(brushedYData);
-% 
-% handles.ecgDWT1 = ecgDWT1;
-% 
-% 
-% plot(handles.QRS1_axes,ecgDWT1); set(handles.QRS1_axes,'XMinorTick','on');
-% Update All figures with Brushed Data
-% figHandles = findall(0,'Type','figure');
-% for ii = 1:length(figHandles)
-%     brDataFig = findobj(figHandles(ii),'-property','BrushData');
-%     set(brDataFig,'BrushData',brDataInd)
-%     hb = findall(figHandles(ii),'Tag','Brushing');
-%     set(hb,'MarkerEdgeColor',[1 0 1]);
-%     % NOTE: I have also tried properties 'MarkerFaceColor' and 'Color', neither worked
-% end
-
-%  guidata(hObject,handles);
-%   function dwt1_pushbutton_Callback(hObject, eventdata, handles)
-%  ecgDWT1=handles.ecgDWT1; 
-%   end
-% brush(figure_handle)
-% ActionPostCallback <function_handle>
-% uiinspect(gca);
-% 
-% brushedIdx = logical(hLine.BrushData);  % logical array
-% brushedXData = hLine.XData(brushedIdx);
-% brushedYData = hLine.YData(brushedIdx);
-% 
-%  hNewVarMenuItem = findall(gcf,'tag','figDataManagerNewVar')
-% 
-%  hNewVarCallback = get(hNewVarMenuItem,'callback');
-% hNewVarCallback = @datamanager.newvar
-%  
-% hNewVarCallback(gcf)   % activate the callback
-% % => set 'ans' as the new variable holding the brushed data
-%  function myfunction(src,event_data)
-%  src         handle to the figure that has been clicked
-%  event_data   object containing event data
-%  end
-%  function myfunction(src,event_data)
-% % src         handle to the figure that has been clicked
-% % event_data   object containing event data
-% % %				    (same as the event data of the
-% % %             'ActionPreCallback' callback)
-% % end
-% function [SelectedData] = GetSelectedData(es,ed)
-% clc;
-% fprintf('Obje üzerinden veri seçimi tamamland?!\n');
-% Handle = findobj(gcf,'-property','BrushData');
-% % a = get(Handle,'BrushData');
-% % end
-
-
 
 
